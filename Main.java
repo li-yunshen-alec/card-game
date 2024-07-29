44e35db6dd39e212bdac4cdcafbfeee8dd14f835
Alec Li <yunshen08@gmail.com>
2024-07-29 00:37:00 -0400

    update cards

--- Main.java
+++ Main.java
@@ -7,13 +7,21 @@ class Main {
     public static void main(String[] args) {
 
         JFrame f = new JFrame("hi");
-        GamePanel p = new GamePanel();
 
-        f.setVisible(true);
-        f.setSize(1300, 1000);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel p = new JPanel(new CardLayout());
+        MainMenu p1 = new MainMenu();
+        GamePanel p2 = new GamePanel();
 
         f.add(p, BorderLayout.CENTER);
+        p.add(p1, "Menu");
+        p.add(p2, "Game");
+
+        f.setVisible(true);
+        f.setSize(1280, 720);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = (CardLayout) p.getLayout();
+        cardLayout.show(p, "Menu");
 
     }
 }
